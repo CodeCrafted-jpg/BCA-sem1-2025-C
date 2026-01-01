d69435c85bc33692bcf114bf008dc4332f1b0a7e
Sayan Mallick <malsayan6@gmail.com>
2026-01-01 22:07:09 +0530

    c

---
+++ conversion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void decToBinary(int n) {
+    int binaryNum[32];
+    int i = 0;
+
+    while (n > 0) {
+        binaryNum[i] = n % 2; 
+        n = n / 2;            
+        i++;
+    }
+
+   
+    printf("Binary: ");
+    for (int j = i - 1; j >= 0; j--)
+        printf("%d", binaryNum[j]);
+}
+
+int main() {
+    int n = 13;
+    decToBinary(n); 
+    return 0;
+}

702a8fb0c2e6be1f960ec3255c70d7a1c97d373a
Sayan Mallick <malsayan6@gmail.com>
2026-01-06 17:04:58 +0530

    class work

---
+++ Advanced/book.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+ struct Book
+{
+  char Name[30];
+  float  price;
+  char author[50];
+  int isbn;
+  
+ 
+
+};
+void Print(struct Book book);
+int main(){
+    struct  Book book1={"Let us see",150.0,"Yashavant Kanetkar",3443};
+    Print(book1);
+
+}
+void Print(struct Book book){
+
+      printf("Name: %s \n", book.Name);
+      printf("Price: %.2f \n" , book.price);
+      printf("Author: %s \n" , book.author);
+      printf("Isbn: %d \n" , book.isbn);
+      
+}

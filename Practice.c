7b26e6cfae7f56b20e9fdef30f7d273e79f919f0
Sayan Mallick <malsayan6@gmail.com>
2026-01-28 22:48:17 +0530

    pc

--- Practice.c
+++ Practice.c
@@ -2,37 +2,51 @@
 
 
 int main(){
-   int arr[100],n,temp;
+   int a[100][100],b[100][100],c,r,sum[100][100];
 
    printf("enter n");
-   scanf("%d", &n);
+   scanf("%d%d", &c,&r);
 
-   for (int i = 0; i < n; i++)
+   for (int i = 0; i < c; i++)
    {
-      scanf("%d" ,&arr[i]);
+    for (int j= 0; j < r; j++)
+    {
+        scanf("%d" ,&a[i][j]); 
+    }
+    
+     
    }
-
-   for (int i = 0; i < n-1; i++)
+    for (int i = 0; i < c; i++)
    {
-     for (int j = i+1; j < n; j++)
-     {
-        if (arr[i]<arr[j])
-        {
-            temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
+    for (int j= 0; j < r; j++)
+    {
+        scanf("%d" ,&b[i][j]); 
+    }
+    
+     
+} 
 
-        }
-        
-     }
+printf("sum: ");
+ for (int i = 0; i < c; i++)
+   {
+    for (int j= 0; j < r; j++)
+    {
+        sum[i][j]=a[i][j]+b[i][j];
+    }
+    
      
    }
 
-   printf("Display");
-   for (int i = 0; i < n; i++)
-   {
-    printf("%d", arr[i]);
-   }
+
+
+     printf("Sum of the two matrices:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("%d\t", sum[i][j]);
+        }
+        printf("\n");
+    }
+
    
    
    
